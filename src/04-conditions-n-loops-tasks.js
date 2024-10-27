1c3a08044e0b1a4b284b619b4cd0304acaa155bf
ŅatalyKisialiova <nataly.kisialiova@gmail.com>
2024-10-27 23:48:55 +0200

    feat: add conditions-n-loops-tasks isInsideCircle, doRectanglesOverlap  resolving

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -137,8 +137,12 @@ function isTriangle(a, b, c) {
  *   { top:20, left:20, width: 20, height: 20 }    =>  false
  *
  */
-function doRectanglesOverlap(/* rect1, rect2 */) {
-  throw new Error('Not implemented');
+function doRectanglesOverlap(rect1, rect2) {
+  if (rect2.top >= rect1.top + rect1.height) return false;
+  if (rect2.left >= rect1.left + rect1.width) return false;
+  if (rect2.top + rect2.height <= rect1.top) return false;
+  if (rect2.left + rect2.width <= rect1.left) return false;
+  return true;
 }
 
 
@@ -168,8 +172,9 @@ function doRectanglesOverlap(/* rect1, rect2 */) {
  *   { center: { x:0, y:0 }, radius:10 },  { x:10, y:10 }   => false
  *
  */
-function isInsideCircle(/* circle, point */) {
-  throw new Error('Not implemented');
+function isInsideCircle(circle, point) {
+  const distanse = (point.x - circle.center.x) ** 2 + (point.y - circle.center.y) ** 2;
+  return circle.radius ** 2 > distanse;
 }
 
 
